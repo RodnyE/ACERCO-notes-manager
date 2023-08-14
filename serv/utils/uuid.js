be909ce6380d1752a445937d8f753fe117ac28f2
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-08-14 01:35:50 -0400

    fix: The get day problem with a one-day delay

--- serv/utils/uuid.js
+++ serv/utils/uuid.js
@@ -10,7 +10,7 @@ module.exports = {
 
         let year = date.getFullYear();
         let month = date.getMonth() + 1; // El método getMonth() retorna valores de 0 a 11, por eso se le suma 1
-        let day = date.getDate();
+        let day = date.getDate() + 1;
  
         if (month < 10) month = '0' + month;
         if (day < 10) day = '0' + day;
