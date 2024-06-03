f30042503e557ccec09e48c3d0f86e2df7116834
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:16:11 -0400

    improve

--- src/ui/Popup.jsx
+++ src/ui/Popup.jsx
@@ -1,24 +1,34 @@
+import PropTypes from 'prop-types';
 
-export default function Popup ({
-    show,
-    color,
-    text,
-    children,
+export default function Popup({
+  show,
+  color = 'primary',
+  text,
+  children,
 }) {
-    return (
-      <div 
-        style={{
-            position: "fixed",
-            top: 0,
-            right: 0,
-        }}
-        className={
-            "alert shadow-sm"
-            + (color ? " alert-" + color : "")
-            + (!show ? " d-none" : "")
-        }
-      >
-        {text || children}
-      </div>
-    )
+  return (
+    <div
+      role="alert"
+      style={{
+        position: 'fixed',
+        top: 0,
+        right: 0,
+      }}
+      className={
+        'alert shadow-sm' +
+        (color ? ' alert-' + color : '') +
+        (!show ? ' d-none' : '')
+      }
+    >
+      {text && <p>{text}</p>}
+      {children && <p>{children}</p>}
+    </div>
+  );
 }
+
+Popup.propTypes = {
+  show: PropTypes.bool,
+  color: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info']),
+  text: PropTypes.node,
+  children: PropTypes.node,
+};
