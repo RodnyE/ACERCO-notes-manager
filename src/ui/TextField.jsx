0f2abc87224d32febdd347e17c4120d5c5c1d91d
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:16:15 -0400

    improve

--- src/ui/TextField.jsx
+++ src/ui/TextField.jsx
@@ -1,19 +1,49 @@
+import PropTypes from 'prop-types';
 
-export default function TextField ({placeholder, onInput, type, value, disabled, color}) {
-    return (
-      <input 
-        className={
-          "form-control bg-transparent time-2" 
-          + (disabled ? " disabled text-secondary" : "")
-          + (color === "success" ? " is-valid" : "")
-          + (color === "danger" ? " is-invalid" : "")
-        }
-        disabled={disabled}
-        type={type}
-        value={value}
-        placeholder={placeholder}
-        
-        onInput={onInput}
-      />
-    );
+export default function TextField({
+  placeholder,
+  onInput,
+  type = 'text',
+  placeholderColor = 'primary',
+  disabled = false,
+  value = '',
+  id,
+}) {
+  const inputClasses = [
+    'form-control',
+    'bg-transparent',
+    'time-2',
+    disabled && 'disabled',
+    `text-${placeholderColor}`,
+  ].join(' ');
+
+  const inputProps = {
+    className: inputClasses,
+    disabled,
+    type,
+    value,
+    placeholder,
+    id,
+    required: placeholder ? true : false,
+    onInput,
+  };
+
+  return <input {...inputProps} />;
 }
+
+TextField.propTypes = {
+  placeholder: PropTypes.string,
+  onInput: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(['text', 'email', 'password']),
+  placeholderColor: PropTypes.oneOf(['primary', 'success', 'danger']),
+  disabled: PropTypes.bool,
+  value: PropTypes.string,
+  id: PropTypes.string,
+};
+
+TextField.defaultProps = {
+  placeholderColor: 'primary',
+  disabled: false,
+  value: '',
+  id: '',
+};
