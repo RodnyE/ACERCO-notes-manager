68d2c7130f7a5debfea47185cb8003643e5144dc
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:17:20 -0400

    improve

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,4 +1,10 @@
+import React from 'react';
 
-export default function MainView () {
-    
+export default function MainView() {
+  return (
+    <div>
+      <h1>Welcome to the Main View!</h1>
+      <p>This is where you can add your main content.</p>
+    </div>
+  );
 }
