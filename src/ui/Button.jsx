d0b3108ff04494464e4920a6beb0e256b5dc45f3
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:16:36 -0400

    improve

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,14 +1,26 @@
+import PropTypes from 'prop-types';
 
-export default function Button ({onClick, children, disabled}) {
-    return (
-      <div 
-        onClick={onClick} 
-        className={
-          "btn btn-primary time-2" 
-          + (disabled ? " disabled" : "")
-        }
-      > 
-        {children} 
-      </div>
-    )
+export default function Button({ onClick, children, type = 'button', disabled, disabledStyle, tabIndex }) {
+  const btnClass = `btn btn-primary time-2 ${disabled ? 'disabled' : ''}`;
+
+  return (
+    <button
+      onClick={onClick}
+      className={btnClass}
+      style={disabled ? disabledStyle : null}
+      type={type}
+      tabIndex={disabled ? -1 : 0}
+    >
+      {children}
+    </button>
+  );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  disabled: PropTypes.bool,
+  disabledStyle: PropTypes.object,
+  tabIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
