5674561493f5022df6e24be0caf413a9cd696732
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:16:50 -0400

    improve

--- serv/router/user.js
+++ serv/router/user.js
@@ -1,14 +1,21 @@
-
 const { DB } = require("../../config");
 const { getDocxJson } = require("../logic/docx-manager");
-const vtmiddleware = require("../middlewares/verify-token");
+const verifyToken = require("../middlewares/verify-token");
 const { Router } = require("express");
 const router = new Router();
 
-router.get("/api/user-sign.png", vtmiddleware());
-router.get("/api/user-sign.png", (req, res) => {
-    let userData = req.userJson;
-    res.sendFile(DB + "/users/" + userData.id + "/sign.png");
+router.get("/api/user-sign.png", verifyToken, (req, res) => {
+  let userData = req.userJson;
+  if (!userData) {
+    return res.status(404).json({ error: "User data not found" });
+  }
+  
+  res.sendFile(DB + "/users/" + userData.id + "/sign.png", (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to send file" });
+    }
+  });
 });
 
 module.exports = router;
