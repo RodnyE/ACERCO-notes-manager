4eb94e17b9f1bfeb1bb4f36b37d2203f678a6686
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:16:33 -0400

    improve

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,78 +1,74 @@
 /**
  * LocalStorage Manager
  */
- 
+
 const stg = {
-    
-    // database
-    // not use directly, instead getData
-    db: (() => {
-        const db = localStorage.getItem("storage");
-        
-        if (db) return JSON.parse(db);
-        return {};
-    })(),
-    
-    
-    
-    /**
-     * Get a stored value 
-     * 
-     * @param  {string} id - name of stored value
-     * @param  {*} dfl - if not stored value, return this
-     * @return {*} stored value or `dfl`
-     */
-    getData (id, dfl) {
-        if (this.existsData(id)) return this.db[id];
-        return dfl;
-    },
-    
-    
-    
-    /**
-     * Set a value 
-     * 
-     * @param  {string} id - name of value
-     * @param  {*} value - a value to store
-     * @return {*} the `value` param
-     */
-    setData (id, value) {
-        this.db[id] = value;
-        this.save();
-        return value;
-    },
-    
-    
-    
-    /**
-     * Remove a specific value
-     *
-     * @param {string} name of value
-     */
-    removeData (id) {
-        delete this.db[id];
-        this.save();
-    },
-    
-    
-    
-    /**
-     * Verify if exist a stored value 
-     * 
-     * @param  {string}  name of value to verify
-     * @return {boolean} if exist or not
-     */
-    existsData (id) {
-        return this.db.hasOwnProperty(id);
-    },
-    
-    
-    /**
-     * Save data to local storage
-     */
-    save () {
-        localStorage.setItem("storage", JSON.stringify(this.db));
+  // Check if localStorage is available
+  isLocalStorageAvailable: () => {
+    try {
+      const x = '__localStorage__test__';
+      localStorage.setItem(x, x);
+      localStorage.removeItem(x);
+      return true;
+    } catch (e) {
+      return false;
     }
+  },
+
+  // Initializes the storage object
+  init: () => {
+    if (!stg.isLocalStorageAvailable()) {
+      console.error('Local storage is not available');
+      return;
+    }
+
+    stg.db = JSON.parse(localStorage.getItem('storage')) || {};
+  },
+
+  // Get a stored value
+  // @param  {string} id - name of stored value
+  // @param  {*} dfl - default value if not stored value
+  // @return {*} stored value or `dfl`
+  getData: (id, dfl = null) => {
+    if (stg.existsData(id)) return stg.db[id];
+    return dfl;
+  },
+
+  // Set a value
+  // @param  {string} id - name of value
+  // @param  {*} value - a value to store
+  // @return {*} the `value` param
+  setData: (id, value) => {
+    stg.db[id] = value;
+    stg.save();
+    return value;
+  },
+
+  // Remove a specific value
+  // @param {string} id - name of value
+  removeData: id => {
+    delete stg.db[id];
+    stg.save();
+  },
+
+  // Verify if exist a stored value
+  // @param  {string} id - name of value to verify
+  // @return {boolean} if exist or not
+  existsData: id => stg.db.hasOwnProperty(id),
+
+  // Save data to local storage
+  save: () => {
+    localStorage.setItem('storage', JSON.stringify(stg.db));
+  },
+
+  // Clear the entire storage
+  clear: () => {
+    localStorage.removeItem('storage');
+    stg.init();
+  }
 }
+
+// Initialize the storage object
+stg.init();
 
 export default stg;
