b30c08384b89f1862a6768ad2b274a0e6d4ef2b2
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-03 18:17:10 -0400

    improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,83 @@
-
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { GlobalContext } from "context"
 import stg from "utils/storage.js"
-
 
 import Popup from "ui/Popup"
 import AdminView from "./views/Admin"
 import LoginView from "./views/Login"
 import GeneratorView from "./views/Generator"
 
+type PopupProps = {
+  color: string;
+  text: string;
+  show: boolean;
+}
 
 export default function App () {
+  // User state
+  const [userName, setUserName] = useState<string>(stg.getData("user-name", ""));
+  const [userPass, setUserPass] = useState<string>(stg.getData("user-pass", ""));
+  const [userToken, setUserToken] = useState<string>(stg.getData("user-token", ""));
+
+  // Popup state 
+  const [popupText, setPopupText] = useState<string>("");
+  const [popupShow, setPopupShow] = useState<boolean>(false);
+  const [popupColor, setPopupColor] = useState<string>("");
+
+  const showPopup = (text: string, color: string) => {
+    setPopupText(text || "Empty");
+    setPopupColor(color || "info");
+    setPopupShow(true);
+    window.setTimeout(() => setPopupShow(false), 3000);
+  }
+
+  // View state
+  const [currentView, setCurrentView] = useState<string>("LoginView");
+
+  // Global context
+  const context = {
+    // storage manager
+    userName, 
+    userPass, 
+    userToken, 
+    setUserName: (name: string) => {
+      setUserName(name);
+      stg.setData("user-name", name);
+    }, 
+    setUserPass: (pass: string) => {
+      setUserPass(pass);
+      stg.setData("user-pass", pass);
+    }, 
+    setUserToken: (token: string) => {
+      setUserToken(token);
+      stg.setData("user-token", token);
+    }, 
     
-    // User state
-    const [userName, setUserName] = useState(stg.getData("user-name", ""));
-    const [userPass, setUserPass] = useState(stg.getData("user-pass", ""));
-    const [userToken, setUserToken] = useState(stg.getData("user-token", ""));
+    // change page
+    currentView,
+    setCurrentView,
     
-    // Popup state 
-    const [popupText, setPopupText] = useState("");
-    const [popupShow, setPopupShow] = useState(false);
-    const [popupColor, setPopupColor] = useState(false); 
-    const showPopup = (text, color) => {
-        setPopupText(text || "Empty");
-        setPopupColor(color || "info");
-        setPopupShow(true);
-        window.setTimeout(() => setPopupShow(false), 3000);
+    // dialogs
+    showPopup,
+  };
+
+  useEffect(() => {
+    if (userToken) {
+      setCurrentView("GeneratorView");
     }
-    
-    // View state
-    const [currentView, setCurrentView] = useState("LoginView");
-    
-    // Global context
-    const context = {
-        // storage manager
-        userName, 
-        userPass, 
-        userToken, 
-        setUserName: (name) => {
-            setUserName(name);
-            stg.setData("user-name", name);
-        }, 
-        setUserPass: (pass) => {
-            setUserPass(pass);
-            stg.setData("user-pass", pass);
-        }, 
-        setUserToken: (token) => {
-            setUserToken(token);
-            stg.setData("user-token", token);
-        }, 
-        
-        // change page
-        currentView,
-        setCurrentView,
-        
-        // dialogs
-        showPopup,
-    };
-    
-    // Render app
-    return (<GlobalContext.Provider value={context}>
-        <AdminView show={currentView === "AdminView"} />
-        <LoginView show={currentView === "LoginView"} />
-        <GeneratorView show={currentView === "GeneratorView"} />
-        
-        <Popup 
-            color={popupColor} 
-            text={popupText} 
-            show={popupShow}
-        />
-    </GlobalContext.Provider>)
+  }, [userToken]);
+
+  return (
+    <GlobalContext.Provider value={context}>
+      {!userToken && <LoginView show={currentView === "LoginView"} />}
+      {userToken && <GeneratorView show={currentView === "GeneratorView"} />}
+      {userToken && <AdminView show={currentView === "AdminView"} />}
+
+      <Popup 
+        color={popupColor} 
+        text={popupText} 
+        show={popupShow}
+      />
+    </GlobalContext.Provider>
+  )
 }
